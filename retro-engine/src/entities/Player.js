dfc3e4c178c6653c9d71a7f0c3d04d0e1e2b9dff
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2026-01-20 19:54:28 -0400

    Tile Movement

--- retro-engine/src/entities/Player.js
+++ retro-engine/src/entities/Player.js
@@ -1,21 +1,70 @@
 import { Entity } from "./Entity.js";
+import { TILE_SIZE } from "../core/constants.js";
 
 export class Player extends Entity {
-  constructor(x, y, input) {
-    super(x, y);
+  constructor(tileX, tileY, input) {
+    super(tileX * TILE_SIZE, tileY * TILE_SIZE);
+
+    this.tileX = tileX;
+    this.tileY = tileY;
+
     this.input = input;
-    this.speed = 60;
+
+    this.isMoving = false;
+    this.moveProgress = 0;
+    this.moveSpeed = 80;
+
+    this.direction = null;
   }
 
   update(delta) {
-    if (this.input.isPressed("ArrowUp")) this.y -= this.speed * delta;
-    if (this.input.isPressed("ArrowDown")) this.y += this.speed * delta;
-    if (this.input.isPressed("ArrowLeft")) this.x -= this.speed * delta;
-    if (this.input.isPressed("ArrowRight")) this.x += this.speed * delta;
+    if (!this.isMoving) {
+      this.handleInput();
+    } else {
+      this.move(delta);
+    }
+  }
+
+  handleInput() {
+    if (this.input.isPressed("ArrowUp")) this.startMove("up");
+    else if (this.input.isPressed("ArrowDown")) this.startMove("down");
+    else if (this.input.isPressed("ArrowLeft")) this.startMove("left");
+    else if (this.input.isPressed("ArrowRight")) this.startMove("right");
+  }
+
+  startMove(direction) {
+    this.direction = direction;
+    this.isMoving = true;
+    this.moveProgress = 0;
+
+    if (direction === "up") this.tileY--;
+    if (direction === "down") this.tileY++;
+    if (direction === "left") this.tileX--;
+    if (direction === "right") this.tileX++;
+  }
+
+  move(delta) {
+    this.moveProgress += this.moveSpeed * delta;
+
+    if (this.moveProgress >= TILE_SIZE) {
+      this.snapToGrid();
+      this.isMoving = false;
+      return;
+    }
+
+    if (this.direction === "up") this.y -= this.moveSpeed * delta;
+    if (this.direction === "down") this.y += this.moveSpeed * delta;
+    if (this.direction === "left") this.x -= this.moveSpeed * delta;
+    if (this.direction === "right") this.x += this.moveSpeed * delta;
+  }
+
+  snapToGrid() {
+    this.x = this.tileX * TILE_SIZE;
+    this.y = this.tileY * TILE_SIZE;
   }
 
   render(ctx) {
     ctx.fillStyle = "black";
-    ctx.fillRect(this.x, this.y, 16, 16);
+    ctx.fillRect(this.x, this.y, TILE_SIZE, TILE_SIZE);
   }
 }
--- retro-engine/src/states/OverworldState.js
+++ retro-engine/src/states/OverworldState.js
@@ -2,7 +2,7 @@ import { Player } from "../entities/Player.js";
 
 export class OverworldState {
   constructor(engine) {
-    this.player = new Player(40, 40, engine.input);
+    this.player = new Player(5, 5, engine.input);
   }
 
   update(delta) {
@@ -12,6 +12,26 @@ export class OverworldState {
   render(ctx) {
     ctx.fillStyle = "#9bbc0f";
     ctx.fillRect(0, 0, 160, 144);
+
+    this.drawGrid(ctx);
     this.player.render(ctx);
+  }
+
+  drawGrid(ctx) {
+    ctx.strokeStyle = "rgba(0,0,0,0.1)";
+
+    for (let x = 0; x <= 160; x += 16) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, 144);
+      ctx.stroke();
+    }
+
+    for (let y = 0; y <= 144; y += 16) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(160, y);
+      ctx.stroke();
+    }
   }
 }
